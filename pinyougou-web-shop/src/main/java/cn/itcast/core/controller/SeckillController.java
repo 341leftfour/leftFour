49e4d567665c9fdc70ccc78564b9a190e3469302
guan <563489894@qq.com>
2019-05-08 16:20:07 +0800

    分页查询

--- pinyougou-web-shop/src/main/java/cn/itcast/core/controller/SeckillController.java
+++ pinyougou-web-shop/src/main/java/cn/itcast/core/controller/SeckillController.java
@@ -1,11 +1,11 @@
 package cn.itcast.core.controller;
 
-import cn.itcast.core.pojo.good.Goods;
+
 import cn.itcast.core.pojo.seckill.SeckillOrder;
 import cn.itcast.core.service.SeckillService;
 import com.alibaba.dubbo.config.annotation.Reference;
 import entity.PageResult;
-import entity.Result;
+
 
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +18,13 @@ public class SeckillController {
     @Reference
     private SeckillService seckillService;
     @RequestMapping("/search")
-    public PageResult search(Integer page, Integer rows, @RequestBody SeckillOrder seckillOrder){
-
-       return seckillService.search(page,rows,seckillOrder);
+    public PageResult search(Integer page, Integer rows, @RequestBody(required = false) SeckillOrder seckillOrder){
+        System.out.println(1111);
+        try {
+            return seckillService.search(page,rows,seckillOrder);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
